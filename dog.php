7ae3f02851647ede838a62e6ab7684617356f9e9
toure <tourekadiy@gmail.com>
2017-08-25 12:23:24 +0200

    new file , guzzle;

--- dog.php
+++ dog.php
@@ -1,1 +1,21 @@
 <?php
+require('vendor/autoload.php');
+
+$c = new \GuzzleHttp\Client();
+$res = $c->request('GET',
+'https://dog.ceo/api/breeds/list/all');
+
+echo $res->getStatusCode();
+echo $res->getBody();
+echo "\n";
+/*
+$request = new \GuzzleHttp\Psr7\Request('GET', 'https://dog.ceo/');
+$promise = $client->sendAsync($request)->then(function ($response) {
+    echo 'I completed!' . $response->getBody();
+});
+$promise->wait();
+*/
+// JSON Parsing
+$obj = json_decode($res->getBody());
+var_dump($obj->message->wolfhound);
+echo "\n";
